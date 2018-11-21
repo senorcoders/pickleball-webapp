ccbd5feb854378b0a57d2c3b8e53605e65ddb0da
bdangla-senorcoders <brian@senorcoders.com>
2018-11-21 10:28:15 -0600

    set up project and add search place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component,NgZone } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+	showMap:boolean=false;
+	lat: number = 11.2600445;
+  	lng: number = -85.8857705;
+  public addrKeys: string[];
+  public addr: object;
+
+  //Method to be invoked everytime we receive a new instance 
+  //of the address object from the onSelect event emitter.
+  setAddress(addrObj) {
+    //We are wrapping this in a zone method to reflect the changes
+    //to the object in the DOM.
+    this.zone.run(() => {
+      this.addr = addrObj;
+      this.addrKeys = Object.keys(addrObj);
+    });
+  }
+  clickMap(event){
+  	this.lat=event.coords.lat;
+  	this.lng=event.coords.lng;
+  }
+  constructor(private zone: NgZone) {
+  	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(position => {
+			this.lat=position.coords.latitude
+			this.lng=position.coords.longitude
+        },)
+  	}
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Http, HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { GooglePlacesDirective } from './directives/google-places.directive';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GooglePlacesDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    HttpClientModule,
+     AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyC6ecF25LKJaY3HpKH0iztKDDRgO60W10A'
+    })
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
