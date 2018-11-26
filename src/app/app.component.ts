6ab94457497bdaac74b7e24aadd7fc63f6801a93
bdangla-senorcoders <brian@senorcoders.com>
2018-11-26 09:51:59 -0600

    Add slider to increase search distance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component,NgZone } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+declare var google:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,29 +10,77 @@ export class AppComponent {
 	showMap:boolean=false;
 	lat: number = 11.2600445;
   	lng: number = -85.8857705;
-  public addrKeys: string[];
-  public addr: object;
-
+	public addrKeys: string[];
+	public addr: object;
+  	type:any='all';
+  	markers:any=[];
+  	radius:number=100;
+  	constructor(private zone: NgZone) {
+  	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(position => {
+			this.lat=position.coords.latitude
+			this.lng=position.coords.longitude
+			//this.markers.push({lat:position.coords.latitude,lng:position.coords.longitude})
+        },)
+  	}
+  }
   //Method to be invoked everytime we receive a new instance 
   //of the address object from the onSelect event emitter.
   setAddress(addrObj) {
     //We are wrapping this in a zone method to reflect the changes
     //to the object in the DOM.
-    this.zone.run(() => {
-      this.addr = addrObj;
-      this.addrKeys = Object.keys(addrObj);
-    });
+  //   this.zone.run(() => {
+  //   	this.lat=addrObj.geometry.lat
+  //   	this.lng=addrObj.geometry.lng
+		// this.addr = addrObj;
+		// this.addrKeys = Object.keys(addrObj);
+  //   });
   }
   clickMap(event){
-  	this.lat=event.coords.lat;
-  	this.lng=event.coords.lng;
+  	this.lat=event.coords.lat
+  	this.lng=event.coords.lng
+  	// this.markers[0]={
+  	// 	lat:event.coords.lat,
+  	// 	lng:event.coords.lng
+  	// }
   }
-  constructor(private zone: NgZone) {
-  	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(position => {
-			this.lat=position.coords.latitude
-			this.lng=position.coords.longitude
-        },)
-  	}
+  changeType(e){
+  	this.type=e
+  }
+  search(){
+    var defaultBounds = new google.maps.LatLng(this.lat, this.lng
+      //new google.maps.LatLng(this.lat, this.lng)
+      //new google.maps.LatLng(51.5073509, -0.12775829999998223)
+    );
+    var options={
+    	location:defaultBounds,
+    	radius:this.radius,
+    	type:['rv_park']
+   	};
+    let service = new google.maps.places.PlacesService(document.getElementById('map'));
+	service.nearbySearch(options,(results,status, pagination)=>{
+		pagination.nextPage();
+	  if (status == google.maps.places.PlacesServiceStatus.OK) {
+	    for (let result of results) {
+	    	this.zone.run(() => {
+	    		this.markers.push({
+	      	lat:result.geometry.location.lat(),
+	      	lng:result.geometry.location.lng()
+	      })
+	    	})
+	    }
+		
+	  }
+	else if(status=="ZERO_RESULTS"){
+		window.alert('No RV Park found')
+	}
+	else{
+		window.alert('Something wrong happened try again')
+	}
+    });
+  }
+  setRadius(e){
+  	this.radius=e
+  	this.search();
   }
 }
--- src/app/directives/google-places.directive.ts
+++ src/app/directives/google-places.directive.ts
@@ -9,6 +9,7 @@ export class GooglePlacesDirective implements OnInit {
   @Output() onSelect: EventEmitter<any> = new EventEmitter();
   @Input() lat: number;
   @Input() lng: number;
+  @Input() types: string;
   @HostListener('click') click(e) {
     this.run()
   }
@@ -24,25 +25,30 @@ export class GooglePlacesDirective implements OnInit {
     //@params: place - Google Autocomplete place object
     //@returns: location_obj - An address object in human readable format
     let location_obj = {};
-    for (let i in place.address_components) {
-      let item = place.address_components[i];
+    location_obj['address']=place.formatted_address;
+    location_obj['phone']=place.formatted_phone_number;
+    location_obj['geometry']={'lat':place.geometry.location.lat(),'lng':place.geometry.location.lng()}
+    location_obj['name']=place.name;
+    location_obj['website']=place.website;
+    // for (let i in place.address_components) {
+    //   let item = place.address_components[i];
       
-      location_obj['formatted_address'] = place.formatted_address;
-      if(item['types'].indexOf("locality") > -1) {
-        location_obj['locality'] = item['long_name']
-      } else if (item['types'].indexOf("administrative_area_level_1") > -1) {
-        location_obj['admin_area_l1'] = item['short_name']
-      } else if (item['types'].indexOf("street_number") > -1) {
-        location_obj['street_number'] = item['short_name']
-      } else if (item['types'].indexOf("route") > -1) {
-        location_obj['route'] = item['long_name']
-      } else if (item['types'].indexOf("country") > -1) {
-        location_obj['country'] = item['long_name']
-      } else if (item['types'].indexOf("postal_code") > -1) {
-        location_obj['postal_code'] = item['short_name']
-      }
+    //   location_obj['formatted_address'] = place.formatted_address;
+    //   if(item['types'].indexOf("locality") > -1) {
+    //     location_obj['locality'] = item['long_name']
+    //   } else if (item['types'].indexOf("administrative_area_level_1") > -1) {
+    //     location_obj['admin_area_l1'] = item['short_name']
+    //   } else if (item['types'].indexOf("street_number") > -1) {
+    //     location_obj['street_number'] = item['short_name']
+    //   } else if (item['types'].indexOf("route") > -1) {
+    //     location_obj['route'] = item['long_name']
+    //   } else if (item['types'].indexOf("country") > -1) {
+    //     location_obj['country'] = item['long_name']
+    //   } else if (item['types'].indexOf("postal_code") > -1) {
+    //     location_obj['postal_code'] = item['short_name']
+    //   }
      
-    }
+    // }
     return location_obj;
   }
 
@@ -55,12 +61,23 @@ export class GooglePlacesDirective implements OnInit {
       new google.maps.LatLng(this.lat, this.lng)
       //new google.maps.LatLng(51.5073509, -0.12775829999998223)
     );
-    var options = {
-      bounds: defaultBounds
-    };
+    var options;
+    if(this.types!='all'){
+      options = {
+        bounds: defaultBounds,
+        types:[this.types]
+      };
+    }
+    else{
+      options= {
+        bounds: defaultBounds
+      };
+    }
+    console.log(options)
     autocomplete = new google.maps.places.Autocomplete(this.element,options);
     //Event listener to monitor place changes in the input
     google.maps.event.addListener(autocomplete, 'place_changed', () => {
+      console.log(autocomplete)
     //Emit the new address object for the updated place
     this.onSelect.emit(this.getFormattedAddress(autocomplete.getPlace()));
     });
