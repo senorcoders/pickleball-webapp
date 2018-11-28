e29ba375299956821b511ec8f92eafe62eb9558e
bdangla-senorcoders <brian@senorcoders.com>
2018-11-28 11:42:00 -0600

    add place information on click marker point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ declare var google:any;
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	showMap:boolean=false;
+	showLocation:boolean=false;
 	lat: number = 11.2600445;
   	lng: number = -85.8857705;
 	public addrKeys: string[];
@@ -17,6 +17,8 @@ export class AppComponent {
   	radius:number=100;
   	query:string;
   	condition:boolean;
+  	showDetails:boolean=false;
+  	index:number;
   	constructor(private zone: NgZone) {
   	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(position => {
@@ -37,12 +39,10 @@ export class AppComponent {
     });
   }
   clickMap(event){
+  	this.showLocation=true;
   	this.lat=event.coords.lat
   	this.lng=event.coords.lng
-  	// this.markers[0]={
-  	// 	lat:event.coords.lat,
-  	// 	lng:event.coords.lng
-  	// }
+  	this.getAddressByLatLng(event.coords.lat, event.coords.lng)
   }
   changeType(e){
   	this.type=e
@@ -70,14 +70,17 @@ export class AppComponent {
     let service = new google.maps.places.PlacesService(document.getElementById('map'));
     
 		service.nearbySearch(options,(results,status, pagination)=>{
-			console.log(results)
 			pagination.nextPage();
 			if (status == google.maps.places.PlacesServiceStatus.OK) {
 			    for (let result of results) {
 			    	this.zone.run(() => {
 			    		this.markers.push({
 			      	lat:result.geometry.location.lat(),
-			      	lng:result.geometry.location.lng()
+			      	lng:result.geometry.location.lng(),
+			      	name:result.name,
+			      	location:result.vicinity,
+			      	photos:result.photos,
+			      	rating:result.rating
 			      })
 			    	})
 			    }
@@ -99,5 +102,28 @@ export class AppComponent {
   setRadius(e){
   	this.radius=e
   	//this.search(this.condition,this.query);
+  }
+  showInfo(i){
+  	this.showDetails=true;
+  	this.index=i;
+  }
+  getAddressByLatLng(lat,lng){
+  	let geocoder = new google.maps.Geocoder;
+  	let latlng = {lat: lat, lng: lng};
+  	geocoder.geocode({'location': latlng}, (results, status) =>{
+    if (status === google.maps.GeocoderStatus.OK) {
+      if (results.length>0) {
+      	let obj={};
+      	obj['geometry']={lat:lat,lng:lng};
+      	obj['name']=results[1].formatted_address;
+      	obj['address']=results[0].formatted_address
+      	this.setAddress(obj)
+      } else {
+      	window.alert('not found')
+      }
+    } else {
+      window.alert('Geocoder failed due to: ' + status);
+    }
+  });
   }
 }
